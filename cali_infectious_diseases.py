0e00d9d8882bdac809e07be195e1ec582bbb9d66
tombra1984 <brossatombra@gmail.com>
2023-10-14 17:00:05 -0600

    commit

--- cali_infectious_diseases.py
+++ cali_infectious_diseases.py
@@ -1,5 +1,10 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import plotly.express as px
+import plotly.io as pio
+import plotly.graph_objects as go
+pio.templates.default = "plotly_white"
+import seaborn as sns
 df= pd.read_csv (r'C:\Users\Tombra\Infectious-diseases-in-California\california infectious disease.csv')
 df_filled = df.fillna(df.mean(numeric_only=True))
 disease_county_grouped = df_filled.groupby(['Disease', 'County'])['Cases'].sum().reset_index()
